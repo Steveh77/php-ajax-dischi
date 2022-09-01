6bfba8df828435184dac5e56c558787242dd30f2
Steveh77 <stederosa76@gmail.com>
2022-09-01 17:54:13 +0200

    Added "seconda versione"

---
+++ v2/js/script.js
@@ -0,0 +1,12 @@
+Vue.config.devtools = true;
+const app = new Vue({
+  el: "#root",
+  data: {
+    discs: [],
+  },
+  mounted() {
+    axios.get("http://localhost/php-ajax-dischi/api/data.php").then((res) => {
+      this.discs = res.data;
+    });
+  },
+});
