883ee826f9d7c589c7180ed3dced1753006d8e59
Steveh77 <stederosa76@gmail.com>
2022-09-01 16:28:14 +0200

    added "prima versione"

--- v1/index.php
+++ v1/index.php
@@ -1,3 +1,5 @@
+<?php include 'data.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.7.4/dist/vue.js"></script>
     <title>vue dischi</title>
@@ -18,7 +21,20 @@
             <i class="fa-brands fa-spotify"></i>
         </nav>
     </header>
-    <main></main>
+    <main>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <?php foreach ($discs as $disc) { ?>
+                    <div class="card">
+                        <img src="<?php echo $disc['poster'] ?>" alt="">
+                        <h3><?php echo $disc['title'] ?></h3>
+                        <h4><?php echo $disc['author'] ?></h4>
+                        <h5><?php echo $disc['year'] ?></h5>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
     <footer></footer>
 </body>
 
